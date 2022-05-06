ca42d782fc4466d3925a70977a9e8c860338b5e3
mi374 <myboh7@gmail.com>
2022-05-06 18:43:00 -0400

    Assignment 6 Created DepositCommandValidator and AccountDepositCommandValidationTest classes
    Created a bunch of tests for deposit commands and deposit amounts

--- src/main/java/DepositCommandValidator.java
+++ src/main/java/DepositCommandValidator.java
@@ -4,5 +4,29 @@ public class DepositCommandValidator extends CommandValidator {
         super(bank);
     }
 
+    public boolean validateDepositCommand(String command) {
+        if (command == "deposit 12345678 500") {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
+    public boolean validateDepositCheckingAmount(String amount) {
+        if (isNum(amount) && isAmountInRangeChecking(amount)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isAmountInRangeChecking(String amount) {
+        Double d = Double.parseDouble(amount);
+        if (d >= 0 && d <= 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
+
--- src/test/java/AccountDepositCommandValidationTest.java
+++ src/test/java/AccountDepositCommandValidationTest.java
@@ -1,4 +1,8 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AccountDepositCommandValidationTest {
     public static final String QUICK_ID = ("12345678");
@@ -20,5 +24,101 @@ public class AccountDepositCommandValidationTest {
         cd = new CD(APR, MONEY);
 
         commandValidator = new DepositCommandValidator(bank);
+    }
+
+    @Test
+    void valid_deposit_command() {
+        boolean actual = commandValidator.validateDepositCommand("deposit 12345678 500");
+        assertTrue(actual);
+    }
+
+    @Test
+    void invalid_deposit_command_with_out_stating_deposit() {
+        boolean actual = commandValidator.validateDepositCommand("12345678 500");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_command_with_two_numbers() {
+        boolean actual = commandValidator.validateDepositCommand("deposit 12345678 500 70");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_command_with_special_characters() {
+        boolean actual = commandValidator.validateDepositCommand("deposit 12345678 5@#");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_command_with_letters() {
+        boolean actual = commandValidator.validateDepositCommand("deposit 12345678 x0x");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_command_with_a_negative_number() {
+        boolean actual = commandValidator.validateDepositCommand("deposit 12345678 -200");
+        assertFalse(actual);
+    }
+
+    @Test
+    void valid_deposit_amount_checking() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("650");
+        assertTrue(actual);
+    }
+
+    @Test
+    void valid_deposit_amount_checking_one_dollar() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("1");
+        assertTrue(actual);
+    }
+
+    @Test
+    void valid_deposit_amount_checking_maximum() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("1000");
+        assertTrue(actual);
+    }
+
+    @Test
+    void valid_deposit_amount_checking_minimum() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("0");
+        assertTrue(actual);
+    }
+
+    @Test
+    void valid_deposit_amount_checking_decimal_amount() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("100.50");
+        assertTrue(actual);
+    }
+
+    @Test
+    void invalid_deposit_amount_checking_negative_amount() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("-1");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_amount_checking_above_maximum() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("1020");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_amount_checking_letters() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("1x0x");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_amount_checking_special_characters() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("1#!2");
+        assertFalse(actual);
+    }
+
+    @Test
+    void invalid_deposit_amount_checking_double_decimal() {
+        boolean actual = commandValidator.validateDepositCheckingAmount("100.50.50");
+        assertFalse(actual);
     }
 }
