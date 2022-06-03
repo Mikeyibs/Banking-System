1c72423ad81adbf3f1ad6e8547cf1b0fb4ab9d0d
mi374 <myboh7@gmail.com>
2022-06-03 12:05:26 -0400

    Post Assignment 8 Changes Added in the transfer command validator.

--- src/main/java/banking/TransferCommandValidator.java
+++ src/main/java/banking/TransferCommandValidator.java
@@ -14,14 +14,35 @@ public class TransferCommandValidator extends CommandValidator {
         return parseString(command, 2);
     }
 
+    public String getAmount(String command) {
+        return parseString(command, 3);
+    }
+
     @Override
     public boolean validate(String command) {
         if (validateAccountExists(getFromId(command)) && validateAccountExists(getToId(command))) {
-            if (validateTransferLength(command)) {
+            if (validateTransferLength(command) && validateWithdrawAmount(getFromId(command), getAmount(command)) &&
+                    validateDepositAmount(getToId(command), getAmount(command))) {
                 return true;
             } else {
                 return false;
             }
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validateWithdrawAmount(String quickId, String amount) {
+        if (isNum(amount)) {
+            return bank.getAccounts().get(quickId).validateWithdrawAmount(amount);
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validateDepositAmount(String quickId, String amount) {
+        if (isNum(amount)) {
+            return bank.getAccounts().get(quickId).validateDepositAmount(amount);
         } else {
             return false;
         }
---
+++ src/test/java/banking/TransferCommandValidatorTest.java
@@ -0,0 +1,328 @@
+package banking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TransferCommandValidatorTest {
+    public static final String QUICK_ID = ("12345678");
+    public static final double APR = (0.06);
+    public static final double MONEY = (2000);
+    public static final String COMMAND = ("transfer 12345678 22334455 200");
+
+    Bank bank;
+    Account checking;
+    Account savings;
+    Account cd;
+    TransferCommandValidator commandValidator;
+    CommandValidator validator;
+    CommandProcessor processor;
+
+    @BeforeEach
+    void setUp() {
+        bank = new Bank();
+        checking = new Checking(APR, 0);
+        savings = new Savings(APR, 0);
+        cd = new CD(APR, MONEY);
+
+        commandValidator = new TransferCommandValidator(bank);
+        validator = new CommandValidator(bank);
+        processor = new CommandProcessor(bank);
+    }
+
+    @Test
+    void valid_get_to_ID() {
+        String transfer = commandValidator.getToId(COMMAND);
+        Assertions.assertEquals("22334455", transfer);
+    }
+
+    @Test
+    void valid_get_from_ID() {
+        String transfer = commandValidator.getFromId(COMMAND);
+        Assertions.assertEquals("12345678", transfer);
+    }
+
+    @Test
+    void valid_account_exists_in_bank() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateAccountExists(QUICK_ID);
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_account_does_not_exist_in_bank() {
+        boolean transfer = commandValidator.validateAccountExists(QUICK_ID);
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_transfer_length() {
+        boolean transfer = commandValidator.validateTransferLength(COMMAND);
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_transfer_length() {
+        boolean transfer = commandValidator.validateTransferLength("transfer 12345678 150");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_transfer_length_three_IDs() {
+        boolean transfer = commandValidator.validateTransferLength("transfer 12345678 22334455 11223344 150");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_withdraw_amount_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "200");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_withdraw_zero_amount_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "0");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_withdraw_max_amount_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "400");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_more_then_max_amount_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "1200");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_special_characters_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "4@@");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_negative_amount_from_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "-200");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_withdraw_amount_from_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "500");
+    }
+
+    @Test
+    void valid_withdraw_zero_amount_from_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "0");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_withdraw_max_amount_from_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "1000");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_more_then_max_amount_from_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "1200");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_special_characters_from_savings_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "4@@");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_withdraw_negative_amount_from_savings_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateWithdrawAmount("12345678", "-200");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_deposit_amount_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "700");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_deposit_zero_amount_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "0");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_deposit_max_amount_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "1000");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_deposit_negative_amount_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "-100");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_deposit_greater_then_max_amount_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "1400");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_attempt_to_deposit_special_characters_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "4@@");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_deposit_amount_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "700");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_deposit_zero_amount_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "0");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_deposit_max_amount_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "2400");
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_deposit_greater_then_max_amount_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "4400");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_deposit_negative_amount_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "-100");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_attempt_to_deposit_special_characters_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+
+        boolean transfer = commandValidator.validateDepositAmount("12345678", "4@@");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void valid_transfer_command_from_checking_account_to_checking_account() {
+        processor.processor("create checking 12345678 1.0");
+        processor.processor("create checking 22334455 1.0");
+        processor.processor("deposit 12345678 700");
+        processor.processor("deposit 22334455 500");
+        boolean transfer = commandValidator.validate(COMMAND);
+
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_transfer_command_from_checking_account_to_savings_account() {
+        processor.processor("create checking 12345678 1.0");
+        processor.processor("create savings 22334455 1.0");
+        processor.processor("deposit 12345678 700");
+        processor.processor("deposit 22334455 500");
+        boolean transfer = commandValidator.validate(COMMAND);
+
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_transfer_command_from_savings_account_to_savings_account() {
+        processor.processor("create savings 12345678 1.0");
+        processor.processor("create savings 22334455 1.0");
+        processor.processor("deposit 12345678 700");
+        processor.processor("deposit 22334455 500");
+        boolean transfer = commandValidator.validate(COMMAND);
+
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void valid_transfer_command_from_savings_account_to_checking_account() {
+        processor.processor("create savings 12345678 1.0");
+        processor.processor("create checking 22334455 1.0");
+        processor.processor("deposit 12345678 700");
+        processor.processor("deposit 22334455 500");
+        boolean transfer = commandValidator.validate(COMMAND);
+
+        Assertions.assertTrue(transfer);
+    }
+
+    @Test
+    void invalid_transfer_from_account_that_does_not_exist() {
+        processor.processor("create checking 12345678 1.0");
+
+        boolean transfer = commandValidator.validate("transfer 22334455 12345678 300");
+        Assertions.assertFalse(transfer);
+    }
+
+    @Test
+    void invalid_transfer_from_checking_account_to_account_that_does_not_exist() {
+        processor.processor("create checking 12345678 1.0");
+        processor.processor("deposit 12345678 500");
+
+        boolean transfer = commandValidator.validate(COMMAND);
+        Assertions.assertFalse(transfer);
+    }
+}
